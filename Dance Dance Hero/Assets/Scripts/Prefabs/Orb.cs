131450fffccc43c52c67e259d0f867d4a9218d98
Nadine Adnane <nadineadnane@gmail.com>
2023-04-22 17:57:47 -0400

    Undo change to Orb.cs

--- Dance Dance Hero/Assets/Scripts/Prefabs/Orb.cs
+++ Dance Dance Hero/Assets/Scripts/Prefabs/Orb.cs
@@ -11,19 +11,6 @@ public class Orb : Item
     //     }
     // }
 
-    MeshRenderer meshRenderer;
-
-    private void Start()
-    {
-        meshRenderer = GetComponent<MeshRenderer>();
-    }
-
-    public void changeColor()
-    {
-        Color randomColor = Random.ColorHSV(0f, 1f, 1f, 1f, 0.5f, 1f);
-        meshRenderer.material.SetColor("_Color", randomColor);
-    }
-
     public override void HandleGrab()
     {
         GameObject.Find("GlobalObject").GetComponent<OrbManager>().HandlePunch();

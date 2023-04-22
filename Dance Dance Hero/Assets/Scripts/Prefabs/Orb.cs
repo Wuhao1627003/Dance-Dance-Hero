2c3c6d704531cf46e213b0b0df3e07ed886aa2e8
Nadine Adnane <nadineadnane@gmail.com>
2023-04-22 17:56:55 -0400

    Added look & feel assets!

--- Dance Dance Hero/Assets/Scripts/Prefabs/Orb.cs
+++ Dance Dance Hero/Assets/Scripts/Prefabs/Orb.cs
@@ -11,6 +11,19 @@ public class Orb : Item
     //     }
     // }
 
+    MeshRenderer meshRenderer;
+
+    private void Start()
+    {
+        meshRenderer = GetComponent<MeshRenderer>();
+    }
+
+    public void changeColor()
+    {
+        Color randomColor = Random.ColorHSV(0f, 1f, 1f, 1f, 0.5f, 1f);
+        meshRenderer.material.SetColor("_Color", randomColor);
+    }
+
     public override void HandleGrab()
     {
         GameObject.Find("GlobalObject").GetComponent<OrbManager>().HandlePunch();

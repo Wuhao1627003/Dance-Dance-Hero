396e96c18478488c3a29b8bd59d28b6431479413
windBlowsAndGoes <haowu.p@outlook.com>
2023-05-04 12:35:17 -0400

    Spawning Patterns

--- Dance Dance Hero/Assets/Scripts/PrefabScripts/Item.cs
+++ Dance Dance Hero/Assets/Scripts/PrefabScripts/Item.cs
@@ -1,37 +1,57 @@
 ﻿using UnityEngine;
 using UnityEngine.XR;
 
+public enum SpawnMethod
+{
+    Comet,
+    Direct,
+    ZigZag
+}
+
 public abstract class Item : MonoBehaviour
 {
-    public bool shootAtPlayer = false;
     public float speed = 0.2f;
-    public Vector3 velocity = Vector3.down;
+    private Vector3 velocity;
+    private Vector3 acceleration;
     private Vector3 camPos;
     public AudioClip punchAudio;
+    public SpawnMethod spawnMethod;
 
     private GameObject globalObj;
 
     void Awake()
     {
-        globalObj = GameObject.Find("GlobalObject");
-        float radius = 1.5f;
-        Vector2 randomPointOnCircle = Random.insideUnitCircle.normalized * radius;
-        camPos = globalObj.GetComponent<ItemManager>().initialCameraPosition;
-        transform.position = new(randomPointOnCircle.x, camPos.y + 3.0f, -1 * Mathf.Abs(randomPointOnCircle.y));
-
-        if (shootAtPlayer)
+        float rand = Random.value;
+        if (rand < 0.6)
         {
-            Vector3 playerCenter = new Vector3(camPos.x, camPos.y / 2, camPos.z);
-            velocity = (playerCenter - transform.position).normalized;
+            SpawnDirect();
         }
-
-        velocity *= speed;
+        else if (rand < 0.8)
+        {
+            SpawnZigZag();
+        }
+        else
+        {
+            SpawnComet();
+        }
     }
 
-    public virtual void FixedUpdate()
+    public void FixedUpdate()
     {
+        velocity += acceleration * Time.fixedDeltaTime;
         transform.position += velocity * Time.fixedDeltaTime;
+
+        if (spawnMethod == SpawnMethod.ZigZag)
+        {
+            if (transform.position.x > 1.5f || transform.position.x < -1.5f)
+            {
+                velocity.x = -velocity.x;
+                acceleration.x = -acceleration.x;
+            }
+        }
     }
+
+    public
 
     void OnTriggerEnter(Collider other)
     {
@@ -81,4 +101,46 @@ public abstract class Item : MonoBehaviour
 
     public abstract void HandleGrab();
     public virtual void HandleEarthCollision() {}
+
+    private void SpawnDirect()
+    {
+        spawnMethod = SpawnMethod.Direct;
+        globalObj = GameObject.Find("GlobalObject");
+        float radius = 1.5f;
+        float theta = Random.Range(0.0f, 2 / 3 * Mathf.PI);
+        camPos = globalObj.GetComponent<ItemManager>().initialCameraPosition;
+        transform.position = new(radius * Mathf.Cos(theta), camPos.y + 3.0f, -radius * Mathf.Sin(theta));
+
+        Vector3 playerCenter = new Vector3(camPos.x, camPos.y / 2, camPos.z);
+        velocity = (playerCenter - transform.position).normalized * speed;
+
+        acceleration = new Vector3(0, -1, 0);
+    }
+
+    private void SpawnZigZag()
+    {
+        spawnMethod = SpawnMethod.ZigZag;
+
+        globalObj = GameObject.Find("GlobalObject");
+        float radius = 1.5f;
+        camPos = globalObj.GetComponent<ItemManager>().initialCameraPosition;
+        transform.position = new(0, camPos.y + 3.0f, -radius);
+
+        bool toLeft = Random.value < 0.5;
+        velocity = (new Vector3(toLeft ? -1 : 1, -1, 1)).normalized * speed;
+
+        acceleration = new Vector3(0, -1, 0);
+    }
+
+    private void SpawnComet()
+    {
+        spawnMethod = SpawnMethod.Comet;
+
+        bool fromLeft = Random.value < 0.5;
+        transform.position = new Vector3(fromLeft ? -10 : 10, 0, -3);
+
+        velocity = new Vector3(fromLeft ? 1 : -1, 1, 1).normalized * speed;
+
+        acceleration = new Vector3(0, 2, 0);
+    }
 }
